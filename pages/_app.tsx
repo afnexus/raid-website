f34ce8f223a688a62e78f56ab58de62f4e42bf05
Natasha Ann <35247553+natashaannn@users.noreply.github.com>
2022-11-21 12:22:53 +0800

    feat(contact): Embed zendesk chat widget

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import Layout from '../components/Layout';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import ZendeskWidget from '../features/contact';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <ChakraProvider>
     <Layout>
       <Component {...pageProps} />
+      <ZendeskWidget/>
     </Layout>
   </ChakraProvider>)
 }
