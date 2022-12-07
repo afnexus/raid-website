d8d98a6649cf395f05b134dc638d721f06339a58
Ervin <ervin_tan@live.com>
2022-12-07 17:48:02 +0800

    Editing InnoFest page

--- pages/innofest.tsx
+++ pages/innofest.tsx
@@ -1,8 +1,15 @@
 import Head from "next/head";
 import InnoFest from "../features/innofest";
 import { Stack } from "@chakra-ui/react";
+// @ts-ignore
+import logo1 from '../public/swift.png';
+import { Box, LinkOverlay } from "@chakra-ui/react";
+import { Image } from '@chakra-ui/react'
 
 export default function Home() {
+
+  const logo = require('../public/swift.png');
+
   return (
     <div>
       <Head>
@@ -13,6 +20,9 @@ export default function Home() {
         />
         <link rel="icon" href="/raid.svg" />
       </Head>
+      <Box style={{padding: 35}}>
+        <Image width='125px' src='https://i.imgur.com/uViUK5S.png' alt='SWiFT' />
+      </Box>
       <Stack overflowX="hidden">
         <InnoFest />
       </Stack>
