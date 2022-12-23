ea0262395294f6f856360f15b67b5dc5c40b47cf
Ervin <ervin_tan@live.com>
2022-12-23 09:11:10 +0800

    Solvathon Drop

--- pages/innofest/index.tsx
+++ pages/innofest/index.tsx
@@ -18,8 +18,13 @@ import {
   Th,
   Thead,
   Tr,
+  Divider
 } from "@chakra-ui/react";
-import { ChevronRightIcon } from "@chakra-ui/icons";
+import { ChevronRightIcon, 
+  CheckIcon,
+  InfoOutlineIcon,
+  PhoneIcon
+} from "@chakra-ui/icons";
 import {
   primary,
   secondary,
@@ -38,7 +43,7 @@ export default function InnoFest(props: LandingBodyProps) {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    const target = new Date("12/22/2022 23:59:59");
+    const target = new Date("1/5/2023 11:59:59");
 
     const Interval = setInterval(() => {
       const now = new Date();
@@ -60,6 +65,7 @@ export default function InnoFest(props: LandingBodyProps) {
 
   return (
     <>
+
       <Stack overflowX="hidden">
         <Box
           w="100vw"
@@ -80,9 +86,127 @@ export default function InnoFest(props: LandingBodyProps) {
               src="https://i.imgur.com/uViUK5S.png"
               alt="SWiFT"
             />
+
+            <Link href="#innofest" fontSize={{ lg: "xl" }}color={nYellow[400]}>
+              <b className="glowtext2">Click here to view InnoFest Details</b>
+            </Link>
+
             <Box
-              marginTop={"0"}
-              textAlign="left"
+              marginTop={5}
+              textAlign="justify"
+              py={{ base: 10, md: 20 }}
+              px={{ base: 5, md: 20 }}
+              boxShadow={"base"}
+              rounded={"3xl"}
+              backgroundColor={primary[800]}
+              w="100%"
+              id="solvathon"
+            >
+              <Heading size={"4xl"} mb={5} textAlign="center" color={nYellow[400]}>
+                <Text fontFamily="Segoe Script" fontSize={{ base: "md", lg: "xl" }}><i>Innofest 22/23 Presents</i></Text>
+                <Text fontFamily="Century Gothic" className="glowtext1">SOLVATHON</Text>
+              </Heading>
+              <Text fontSize={{ base: "md", lg: "2xl" }} mb={5}>
+                The ideas of our innovators have solved many pain points of past.
+              </Text>
+              <Grid
+                templateColumns={{
+                  base: "repeat(2, 1fr) ",
+                  xl: "repeat(3, 1fr)",
+                }}
+                gap={3}
+                backgroundColor={primary[800]}
+                mb={18}
+                position="relative"
+              >
+                <GridItem w="75%" key={0} margin="auto">
+                    <Image align="center" src={"https://i.gyazo.com/9da0564863849505a6f069d2b48dc820.png"} width="100%" alt="" />
+                    <Text fontSize={{ lg: "md" }} align="center">Digitising paper processes: MT Fleet App</Text>
+                </GridItem>
+                <GridItem w="75%" key={1} margin="auto">
+                    <Image align="center" src={"https://i.gyazo.com/992a564c04fcb42a217d4d7fb007eb4b.png"} width="100%" alt="" />
+                    <Text fontSize={{ lg: "md" }} align="center">Saving Manpower: Wingtip Sensors</Text>
+                </GridItem>
+                <GridItem w="75%" key={2} margin="auto">
+                    <Image align="center" src={"https://i.gyazo.com/6a703c5603bb680d393def72094effc3.png"} width="100%" alt="" />
+                    <Image align="center" src={"https://i.gyazo.com/bc8cd22c67704ed00f3f4a933178a361.png"} width="100%" alt="" />
+                    <Text fontSize={{ lg: "md" }} align="center">No more scratching or removal: CDA Sticker</Text>
+                </GridItem>
+              </Grid>
+              <Text fontSize={{ base: "md", lg: "2xl" }} mb={5}>
+                In our efforts to <b><i>#makeinnovationjoyful</i></b>, <Text as="span" color={nYellow[400]}>SW<i>i</i>FT</Text>{" "} wants to hear your pain
+                point AND partner you to solve the pain point through innovation!
+              </Text>
+              <Box
+              text-align="justify"
+              py={{ base: 10, md: 5 }}
+              px={{ base: 5, md: 5 }}
+              boxShadow={"base"}
+              rounded={"3xl"}
+              backgroundColor={primary[600]}
+              w="100%"
+              id="solvathon"
+              mb={5}
+              >
+                <Heading size={"lg"} mb={5} color={nYellow[400]} className="glowtext3">
+                  What you need to do?
+                </Heading>
+                <List spacing={3} >
+                  <ListItem>
+                    <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
+                    Bookmark <a href="http://localhost:3000/innofest">this URL</a> or join our SW<i>i</i>FT Telegram Channel.
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
+                    <b className="glowtext2">Jan 2023</b>: Lookout for updates on this page, Telegram or OSN on how to submit your entry for SOLVATHON.
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
+                    <b className="glowtext2">9 Jan to 20 Jan 2023</b>: Enter the portal most suited for you and submit your entry with the following details:
+                    <List spacing={1} >
+                      <ListItem>&emsp;&emsp;<ListIcon as={CheckIcon} />The pain point</ListItem>
+                      <ListItem>&emsp;&emsp;<ListIcon as={InfoOutlineIcon} />A proposed innovative solution</ListItem>
+                      <ListItem>&emsp;&emsp;<ListIcon as={PhoneIcon} />Your contact details</ListItem>
+                    </List>
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
+                    <b className="glowtext2">6 Feb 2023</b> onwards: Lookout for the <Text as="span" color={nYellow[400]}>SOLVATHON</Text>{" "} poll
+                    and up-vote the entry you want to see SOLVED in the year ahead.
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
+                    <b className="glowtext2">23 Feb 2023</b>: Top 3 voted entries stand to win attractive prizes at InnoFest!
+                  </ListItem>
+                  <Text fontSize={{ base: "md", lg: "lg" }} textAlign="center">
+                    <b>The Winning Entry of <Text as="span" color={nYellow[400]}>SOLVATHON</Text>{" "} will be announced and led by SW<i>i</i>FT Office to be SOLVED in 2023!</b>
+                  </Text>
+                </List>
+              </Box>
+              <Text fontSize={{ base: "md", lg: "2xl" }} mb={5}>
+                Join us on our <b>Telegram Channel</b> or stay tuned to this page!
+              </Text>
+              <div>
+              <Image
+                    src="https://i.imgur.com/PuTtR9b.png"
+                    width="40px"
+                    float="left"
+                    alt=""
+              ></Image>
+              <Text fontSize={{ base: "md", lg: "2xl" }}>
+                <i>Psst</i>: Stay tuned also for <b>exciting courses</b> such as <i>Design Thinking</i>, <i>3D printing</i> and <i>Chatbot
+                Development</i> to <b><i>#upskill</i></b> your innovation quotient!
+              </Text>
+              </div>
+              <Text fontSize={{ base: "md", lg: "2xl" }}  mb={-5} textAlign="center" >
+              JAN <b className="glowtext1">9</b> 2023
+              </Text>
+              <Text fontSize={{ base: "md", lg: "2xl" }} textAlign="center"><b><i>#makinginnovationjoyful</i></b></Text>
+            </Box>
+
+            <Box
+              marginTop={"24"}
+              textAlign="justify"
               py={{ base: 10, md: 20 }}
               px={{ base: 5, md: 20 }}
               boxShadow={"base"}
@@ -90,13 +214,14 @@ export default function InnoFest(props: LandingBodyProps) {
               backgroundColor={primary[800]}
               w="100%"
               maxW="90vw"
+              id="innofest"
             >
-              <Heading size={"3xl"} mb={10} textAlign="center" color={nYellow[400]}>
+              <Heading size={"3xl"} mb={10} textAlign="center" color={nYellow[400]} className="glowtext3">
                 InnoFest 22/23
                 <br />
               </Heading>
 
-              <Text fontSize={{ base: "md", lg: "xl" }} mb={5}>
+              <Text fontSize={{ base: "md", lg: "2xl" }} mb={5}>
                 <Text as="span" color={nYellow[400]}>
                   SW<i>i</i>FT
                 </Text>{" "}
@@ -109,7 +234,7 @@ export default function InnoFest(props: LandingBodyProps) {
                 innovation movement and charts the way ahead for our Innovation
                 journey!
               </Text>
-              <Text fontSize={{ base: "md", lg: "xl" }} mb={5}>
+              <Text fontSize={{ base: "md", lg: "2xl" }} mb={5}>
                 The events for this InnoFest have been specially curated for us
                 to discover together how we can <b>“Make Innovation Joyful”</b>.
                 Here is a glimpse of what you can look forward to!
@@ -135,8 +260,8 @@ export default function InnoFest(props: LandingBodyProps) {
                     <Tr>
                       <Td>Jan 2023 to Feb 2023</Td>
                       <Td>
-                        <Link href="#solvathon" color={nYellow[400]}>
-                          <b>The Solva-thon</b>
+                        <Link href="#solvathon" color={nYellow[400]} className="glowtext2">
+                          <b>The Solvathon</b>
                         </Link>
                       </Td>
                     </Tr>
@@ -222,74 +347,7 @@ export default function InnoFest(props: LandingBodyProps) {
                 ))}
               </Grid>
             </Box>
-            <Box
-              marginTop={24}
-              textAlign="left"
-              py={{ base: 10, md: 20 }}
-              px={{ base: 5, md: 20 }}
-              boxShadow={"base"}
-              rounded={"3xl"}
-              backgroundColor={primary[800]}
-              w="100%"
-              id="solvathon"
-            >
-              <Heading size={"2xl"} mb={5} textAlign="center" color={nYellow[400]}>
-                Solva-thon
-              </Heading>
-              <Text fontSize={{ base: "md", lg: "xl" }} mb={5}>
-                Have a pain point you want to see solved by innovation but have
-                not been heard? Have a process, physical or digital innovation
-                idea that can address pain points? We want to hear from you!
-              </Text>
-              <Text fontSize={{ base: "md", lg: "xl" }} mb={2}>
-                In the lead up to InnoFest 22/23, SW<i>i</i>FT will be running
-                the <b>Solva-thon</b> - A campaign to source for innovative
-                ideas to SOLVE pain points from our people!
-              </Text>
-              <Image
-                src="https://eventornado-files.ams3.cdn.digitaloceanspaces.com/blogposts/1/the-rise-of-online-hackathons.png"
-                // height="300px"
-                mb={5}
-                alt="hackathon"
-              />
-              <Heading size={"lg"} mb={5} color={nYellow[400]}>
-                Event Details
-              </Heading>
-              <List spacing={3}>
-                <ListItem>
-                  <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
-                  From <b>9 Jan to 20 Jan 2023</b>, portals will be open (on OSN
-                  and through this page) for you to drop your pain point and the
-                  idea to solve it.
-                </ListItem>
-                <ListItem>
-                  <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
-                  Top ideas to solve pain points will then be put up on a poll
-                  in February for you to vote what you want to see solved most!
-                </ListItem>
-                <ListItem>
-                  <ListIcon as={ChevronRightIcon} color={nGreen[400]} />
-                  <b>Top 3</b> from the poll will be awarded prizes at InnoFest
-                  22/23 with the winning solution given dedicated resources from
-                  SW<i>i</i>FT to lead and solve the pain point.
-                </ListItem>
-                <ListItem>
-                  <ListIcon
-                    as={ChevronRightIcon}
-                    color={nGreen[400]}
-                    float="left"
-                  />
-                  <Image
-                    src="https://i.imgur.com/PuTtR9b.png"
-                    width="22px"
-                    float="left"
-                    alt=""
-                  ></Image>
-                  <i>Psst</i>: Join SW<i>i</i>FT&apos;s Telegram Channel to be
-                  eligible for voting and to win prizes!
-                </ListItem>
-              </List>
-            </Box>
+            
             <Box
               marginTop={24}
               mb={48}
